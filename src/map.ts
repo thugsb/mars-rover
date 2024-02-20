25b96ef6a6fd62dea960f41a1f370e44234ca439
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-02-20 10:08:40 +0000

    Feat: Rover now shown by coloured arrows

--- src/map.ts
+++ src/map.ts
@@ -1,12 +1,32 @@
 import { Map, RoverPosition } from "./setup.types";
+const clc = require("cli-color");
 
 export function generateRoverMap(
   plateauMap: Map,
   { x, y, d }: RoverPosition
 ): Map {
+  let arrow = "?";
+  switch (d) {
+    case "N":
+      arrow = "↑";
+      break;
+    case "S":
+      arrow = "↓";
+      break;
+    case "E":
+      arrow = "→";
+      break;
+    case "W":
+      arrow = "←";
+      break;
+
+    default:
+      break;
+  }
   const outputMap = [...plateauMap];
   const row = outputMap[y];
-  const newRow = row.substring(0, x) + d + row.substring(x + 1);
+  const newRow =
+    row.substring(0, x) + clc.bgCyanBright.red(arrow) + row.substring(x + 1);
   outputMap[y] = newRow;
   return outputMap;
 }
