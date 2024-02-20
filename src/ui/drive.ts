6fe2fe7b2ab6949a0e57afc6fe87d1bbbbf90274
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-02-20 22:45:29 +0000

    Feat: Give UI user a warning when a rover refuses instructions

--- src/ui/drive.ts
+++ src/ui/drive.ts
@@ -1,15 +1,24 @@
 import { rl, d } from "../index";
 import { moveRover } from "../maneuver";
 import { drawPlateau } from "./plateau";
+import { getNewPosition, terrainIsNavigable } from "../move";
 
 export function driveAround() {
   rl.question(
     "What instructions will you give to rover now? ",
     (input: string) => {
       const invalidInstructions: string[] = [];
+      const refusedInstructions: string[] = [];
       for (let i = 0; i < input.length; i++) {
         const instr = input.charAt(i).toUpperCase();
-        if (instr === "M" || instr === "R" || instr === "L") {
+        if (instr === "M") {
+          const targetCoords = getNewPosition(d.position);
+          if (terrainIsNavigable(targetCoords.x, targetCoords.y, d.map)) {
+            d.position = getNewPosition(d.position);
+          } else {
+            refusedInstructions.push(instr);
+          }
+        } else if (instr === "R" || instr === "L") {
           d.position = moveRover(instr, d.position, d.map);
         } else {
           invalidInstructions.push(instr);
@@ -22,7 +31,18 @@ export function driveAround() {
             invalidInstructions.join(",")
         );
       }
-      console.log(`The last set of instructions given were ${input}.`);
+      if (refusedInstructions.length > 0) {
+        console.log(
+          "WARNING: the following instructions were refused: " +
+            refusedInstructions.join(",")
+        );
+      }
+      console.log(
+        `The last set of instructions given were ${input
+          .toUpperCase()
+          .split("")
+          .join(",")}.`
+      );
       driveAround();
     }
   );
